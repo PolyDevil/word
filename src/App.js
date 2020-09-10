70eb4653d4a0e603edbaa9fdf822d8c7ac92b7c7
PolyDevil <polydevil@cocaine.ninja>
2020-09-10 16:57:49 +0300

    Animate word appearance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Flipper, Flipped } from 'react-flip-toolkit';
 import './App.css';
 
+const placeholder = Array.from('placeholder');
+
+const option1 = Array.from('word');
+const option2 = Array.from('exe');
+
+const staggerConfig = {
+  delayedAnimation: {
+    speed: .1,
+  },
+};
+
+const spring = {
+  stiffness: 300,
+  damping: 14,
+};
+
 function App() {
+  const [data1, setData1] = useState(option1);
+  const [data2, setData2] = useState(option2);
+  const [index, setIndex] = useState(0);
+  const swap = () => {
+    if (index === 0) {
+      setData1(option2);
+      setData2(option1);
+      setIndex(1);
+    } else {
+      setData1(option1);
+      setData2(option2);
+      setIndex(0);
+    }
+    setShow(false);
+  };
+
+  const [show, setShow] = useState(false);
+  const toggle = () => {
+    console.log('toggle', show);
+    if (show) {
+      swap();
+    } else {
+      setShow(!show);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="Sentence" onClick={toggle}>
+        <p className="Placeholder">
+          {placeholder.map((d, i) => (
+            <span key={i + d} className="Letter">
+              {d}
+            </span>
+          ))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <Flipper
+          flipKey={[show, index].join("")}
         >
-          Learn React
-        </a>
-      </header>
+          <p className="Word">
+            {data1.map((d, i) => (
+              <Flipped key={i + d} flipId={i + d}>
+                <del className={`Letter ${show ? '--out' : ''}`}>
+                  {d}
+                </del>
+              </Flipped>
+            ))}
+          </p>
+        </Flipper>
+
+        <Flipper
+          flipKey={[show, index].join("")}
+          staggerConfig={staggerConfig}
+          spring={spring}
+        >
+          <p className="Word">
+            {data2.map((d, i) => (
+              <Flipped key={i + d} flipId={i + d} stagger="delayedAnimation">
+                <ins className={`Letter ${show ? '' : '--initial'}`}>
+                  {d}
+                </ins>
+              </Flipped>
+            ))}
+          </p>
+        </Flipper>
+      </div>
     </div>
   );
 }
