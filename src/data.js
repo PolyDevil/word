d6fee04c9b1ea6d175a0c667b914be3e274bcb0f
PolyDevil <polydevil@cocaine.ninja>
2020-09-11 16:38:56 +0300

    Extend vocabulary

--- src/data.js
+++ src/data.js
@@ -114,6 +114,166 @@ const data = [
       'nonavailability',
     ],
   },
+
+  {
+    options: [
+      'telecommunication',
+      'decontamination',
+      'discontinuation',
+      'instrumentation',
+    ],
+  },
+
+  {
+    options: [
+      'reconfiguration',
+      'standardization',
+      'administration',
+      'classification',
+    ],
+  },
+
+  {
+    options: [
+      'discrimination',
+      'identification',
+      'implementation',
+      'multiplication',
+    ],
+  },
+
+  {
+    options: [
+      'recapitulation',
+      'recommendation',
+      'rehabilitation',
+      'specialization',
+      'transportation',
+    ],
+  },
+
+  {
+    options: [
+      'accommodation',
+      'appropriation',
+      'authorization',
+      'certification',
+      'communication',
+    ],
+  },
+
+  {
+    options: [
+      'concentration',
+      'configuration',
+      'consideration',
+      'consolidation',
+    ],
+  },
+
+  {
+    options: [
+      'contamination',
+      'demonstration',
+      'dissemination',
+      'documentation',
+    ],
+  },
+
+  {
+    options: [
+      'interrelation',
+      'investigation',
+      'participation',
+      'qualification',
+      'recombination',
+      'specification',
+      'stabilization',
+    ],
+  },
+
+  {
+    options: [
+      'abbreviation',
+      'acceleration',
+      'accumulation',
+      'anticipation',
+      'apprehension',
+      'augmentation',
+    ],
+  },
+
+  {
+    options: [
+      'commendation',
+      'compensation',
+      'condensation',
+      'constitution',
+      'construction',
+    ],
+  },
+
+  {
+    options: [
+      'contribution',
+      'coordination',
+      'distribution',
+      'facilitation',
+      'illustration',
+    ],
+  },
+
+  {
+    options: [
+      'implantation',
+      'installation',
+      'interruption',
+      'introduction',
+    ],
+  },
+
+  {
+    options: [
+      'jurisdiction',
+      'modification',
+      'organization',
+      'presentation',
+      'preservation',
+      'reproduction',
+    ],
+  },
+
+  {
+    options: [
+      'transmission',
+      'verification',
+      'acquisition',
+      'aggravation',
+      'alternation',
+      'application',
+      'calculation',
+    ],
+  },
+
+  {
+    options: [
+      'calibration',
+      'circulation',
+      'combination',
+      'competition',
+      'composition',
+    ],
+  },
+
+  {
+    options: [
+      'compression',
+      'computation',
+      'conjunction',
+      'correlation',
+      'dereliction',
+    ],
+  },
 ];
 
 export const wordList = data.map(({ options }) => ({
