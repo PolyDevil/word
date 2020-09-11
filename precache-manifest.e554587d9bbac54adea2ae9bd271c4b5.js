a59330f375f3c08dbb062e69e6d69d9205b71ca3
PolyDevil <polydevil@cocaine.ninja>
2020-09-11 16:07:03 +0300

    Updates

---
+++ precache-manifest.e554587d9bbac54adea2ae9bd271c4b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f593e1d77866f865d452f3e408e437",
+    "url": "/word/index.html"
+  },
+  {
+    "revision": "a7b4df23859b18781b21",
+    "url": "/word/static/css/main.cdcd620e.chunk.css"
+  },
+  {
+    "revision": "e0a13b3377dfad358bfa",
+    "url": "/word/static/js/2.c162ecec.chunk.js"
+  },
+  {
+    "revision": "9958780d182350cee9cc1b612e70eb1e",
+    "url": "/word/static/js/2.c162ecec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b4df23859b18781b21",
+    "url": "/word/static/js/main.bae6d4a5.chunk.js"
+  },
+  {
+    "revision": "fdbd1a9a0bf89314b278",
+    "url": "/word/static/js/runtime-main.ac807fef.js"
+  }
+]);
